86b156bd40bc0658f157e7454c1cd13e6134b878
maxjamin <maxjamin@gmail.com>
2019-01-19 14:31:16 -0700

    working on main

--- web/cart.php
+++ web/cart.php
@@ -77,8 +77,6 @@ session_start();
   		{echo $purchased01 . " " . $numberOfProducts01 . " of this product";}
   	?>
   	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-  		Add Amount:<input type="text" name="oneAdd" value="">
-  		Sub Amount<input type="text" name="oneSub" value="">
   		<input type="submit" name="one" value="Change"><br>
   	</form>
   </div>
