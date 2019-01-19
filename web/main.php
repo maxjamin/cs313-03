de48ca24027df946139751c74dd5ab93c4d59ce1
maxjamin <maxjamin@gmail.com>
2019-01-19 11:51:49 -0700

    added buttons

--- web/main.php
+++ web/main.php
@@ -27,13 +27,13 @@
 		if(empty($_POST["quantityOne"])) {
 			$product1Err = "Please enter a number";
 		} else {
-			if(filter_var($_POST["quantityOne"], FILTER_VALIDATE_INT)){
+			/*if(filter_var($_POST["quantityOne"], FILTER_VALIDATE_INT)){
 				//collect values of inputs from form
 				$product1 = $_POST["quantityOne"];
 			}
 			else
 				{ $product1Err = "Please enter a number" }
-		
+		*/
 		}
 
 	}
