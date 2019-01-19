c0196c7472ff1a126ea21ea6b4a3ca7e24d5f844
maxjamin <maxjamin@gmail.com>
2019-01-19 11:05:35 -0700

    added buttons

--- web/main.php
+++ web/main.php
@@ -20,33 +20,44 @@
 </div>
 <br>
 
+<?php
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+		//collect values of inputs from form
+		$product1 = $_REQUEST['quantityOne']
+		echo $product1
+	}
+
+?>
+
  <div class="row">
   <div class="column" style="background-color:#ccc;">
   	<h3>Product 1</h3>
   	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-		Quantity<input type="text" name="name"><br>
-		<input type="submit" value="one" name="Add to Cart">
+		Quantity<input type="text" name="quantityOne"><br>
+		<input type="submit" name="one" name="Add to Cart">
 	</form>
   </div>
   <div class="column" style="background-color:#ddd;">
   	<h3>Product 2</h3>
   	<form action="welcome_get.php" method="post">
-		Quantity<input type="text" name="name"><br>
-		<input type="submit" value="two" name="Add to Cart">
+		Quantity<input type="text" name="quantityTwo"><br>
+		<input type="submit" name="two" name="Add to Cart">
 	</form>
   </div>
   <div class="column" style="background-color:#ddd;">
   	<h3>Product 3</h3>
   	<form action="welcome_get.php" method="post">
-		Quantity<input type="text" name="name"><br>
-		<input type="submit" value="three" name="Add to Cart">
+		Quantity<input type="text" name="quantityThree"><br>
+		<input type="submit" name="three" name="Add to Cart">
 	</form>
   </div>
   <div class="column" style="background-color:#ddd;">
   	<h3>Product 4</h3>
   	<form action="welcome_get.php" method="post">
-		Quantity<input type="text" name="name"><br>
-		<input type="submit" value="four" name="Add to Cart">
+		Quantity<input type="text" name="quantityFour"><br>
+		<input type="submit" name="four" name="Add to Cart">
 	</form>
   </div>
 </div> 
