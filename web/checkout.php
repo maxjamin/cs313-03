3b79443b5d780078c6fd1d1d364e9a320e08e7e9
maxjamin <maxjamin@gmail.com>
2019-01-19 14:48:00 -0700

    working on main

--- web/checkout.php
+++ web/checkout.php
@@ -26,11 +26,12 @@
 	if (empty($_POST["email"])) {
     $emailErr = "Email is required";
   } else {
-    	$email = test_input($_POST["email"]);
-    	// check if e-mail address is well-formed
     	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
       		$emailErr = "Invalid email format";
+      		echo "Error";
     	}
+    	else
+    	{ $email = test_input($_POST["email"]); }
   }
 
 
