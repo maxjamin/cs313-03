75895fbd620e5ec5d667e5f9443a2dfda6b7f0c1
maxjamin <maxjamin@gmail.com>
2019-01-19 15:12:02 -0700

    working on main

--- web/checkout.php
+++ web/checkout.php
@@ -20,8 +20,8 @@
 
 <?php
 
-	$emailErr = $zipErr = "";
-	$email= $zip = "";
+	$emailErr = $zipErr = $addressErr = "";
+	$email= $zip = $address = "";
 
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
@@ -38,8 +38,17 @@
 		    }
 	  	}
 
+	  	if(empty($_POST["address"])){
+	  		$addressErr = "please enter an adress";
+	  	}
+	  	else{
+	  		$address = test_input($_POST["address"]);
+	  	}
+
+
+	  	//check for only numbers or empty
 	  	if(empty($_POST["zip"])) {
-			$zipErr = "Please enter a number";
+			$zipErr = "Please enter a zip";
 		} else {
 			if(!filter_var($_POST["zip"], FILTER_VALIDATE_INT) === false){
 				//collect values of inputs from form
@@ -53,13 +62,21 @@
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" >
 	Email:<input type="text" name="email" >
 	<span class="error"><?php echo $emailErr;?></span><br>
-	Adress:<input type="text" name="Adress"><br>
+	Adress:<input type="text" name="Address"><br>
+	<span class="error"><?php echo $addressErr;?></span><br>
 	State<input type="text" name="State"><br>
 	Zip:<input type="text" name="zip"><br>
 	<span class="error"><?php echo $zipErr;?></span>
 	<input type="submit" name="one" name="Add to Cart"><br>
 </form>
 
+
+<?php
+echo $email;
+echo $zip;
+echo $address;
+
+?>
 
 </body>
 </html>
